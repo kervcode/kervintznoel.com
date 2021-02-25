be379426f4b2c6e81c9e4eeb0a96c55b0dfa6d68
Kervcodes-desktop <Kervintz84@hotmail.com>
2021-02-25 18:12:21 -0500

    code parts to grab image from public/image

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,19 @@
 import react from 'react';
 import "./project.styles.css"
+import { Link } from 'react-router-dom';
 
 
 const Projects = ({ name, description }) => {
+    console.log(name)
     return (
         <div className="projects">
-            <h2>{name}</h2>
-            <div className="project">
-                <p className="project_description">{description} text</p>
-                <img src={process.env.PUBLIC_URL + "image/form-small.png"} alt="t" className="project_image" />
-            </div>
+            <Link to={name}>
+                <h2>{name}</h2>
+                <div className="project">
+                    <p className="project_description">{description} text</p>
+                    <img src={process.env.PUBLIC_URL + "image/" + name +".jpg"} alt="t" className="project_image" />
+                </div>
+            </Link>
         </div>
     )
 }

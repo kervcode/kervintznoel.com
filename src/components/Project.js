2b24123ff4f5cd6a20cf8febe7503925105c2118
Kervintz Noel <kervintz84@hotmail.com>
2021-03-07 00:48:56 -0500

    adding media query for project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,21 +7,21 @@ import detailsPage from './detailsPage';
 const Projects = ({ name, description }) => {
     console.log(name)
     return (
-        <div className="projects">
-        <div>
-            <h2>{name}</h2>
-        </div>
+        <div className="projectContainer">
+            <div>
+                <h2>{name}</h2>
+            </div>
                 
-                <div className="project">
-                    <p className="project_description">{description} text</p>
-                    <img src={process.env.PUBLIC_URL + "image/" + name +".jpg"} alt="t" className="project_image" />
-                </div>
+            <div className="project">
+                <p className="project_description">{description} text</p>
+                <img src={process.env.PUBLIC_URL + "image/" + name +".jpg"} alt="t" className="project_image" />
+            </div>
             <Link to="/projects">
             <button className="button">Live Project</button>
-          </Link>
-          <Link to="/contact" >
-            <button className="button">GitHub</button>
-          </Link>
+            </Link>
+            <Link to="/contact" >
+                <button className="button">GitHub</button>
+            </Link>
         </div>
     )
 }

d81cae81c44c671908ed8a468591390cff71ba5f
Kervcodes-desktop <Kervintz84@hotmail.com>
2021-03-19 00:57:29 -0400

    made menu responsive

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,19 +4,24 @@ import { Link } from 'react-router-dom';
 import { motion } from "framer-motion"
 
 const Menu = () => {
+  
   const [open, setOpen] = useState("close");
+  const [ visible, setVisible] = useState("open");
+  
 
   return (
     <div className='menu'> 
       <span className="logo">KN </span>
       
-      <motion.div className={open}
-        
+      <motion.div className={"list " + open}
+        initial={{x: 100}}
+        animate={{x: 0}}
+        transition={{delay: 1.5}}
       >
         <Link to="/" onClick= { () => setOpen("close")}>Home</Link>
         <Link to="/projects" onClick= { () => setOpen("close")}>Projects</Link>
-        <Link to="/blog" onClick= { () => setOpen("close")}>Blog</Link>
-        <Link to="/Contact" onClick= { () => setOpen("close")}>Contact</Link>
+        {/* <Link to="/blog" onClick= { () => setOpen("close")}>Blog</Link> */}
+        {/* <Link to="/Contact" onClick= { () => setOpen("close")}>Contact</Link> */}
       </motion.div>
       
       <div className="burger" onClick={() => setOpen(open === "close" ? "open" : "close")}>

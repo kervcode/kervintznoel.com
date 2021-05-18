545db7f73eff075ae0361eea0f7280374876047c
Kervcodes-desktop <Kervintz84@hotmail.com>
2021-05-18 10:13:12 -0400

    reimplementing form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,143 +1,83 @@
-import React, { Component } from 'react';
-import * as emailjs from 'emailjs-com';
+import React, { useState } from 'react';
 import "./contact.styles.css";
 import axios from 'axios';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 
-class Contact extends Component {
-  state = {
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
-    disabled: false,
-    emailSent: null,
-    errors: [],
+function Contact() {
+  const [sent, setSent] = useState(false)
+  const [text, setText] = useState("")
+  const [error, setError] = useState("")
+  
+  const handleSubmit = async() => {
+    setSent(true)
+    
+    try {
+      await axios.post("http://localhost:3003/send_mail", {
+        text
+      })
+    } catch (error) {
+      console.log(error)
+    }
   } 
- 
-  change = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    this.setState(() => {
-      return {
-        [name]: value
-      };
-    });
-    
-    console.log(value)
-  }
-  
-  handleSubmit = (e) => {
-    e.preventDefault();
-    
-    this.setState({
-        disable: true
-    })
-    
-    axios.post('https://kervintznoel-api.herokuapp.com/api/contact')
-            .then(res => {
-                console.log(res.data)
-                if(res.data.success) {
-                    this.setState({
-                        disabled: false,
-                        emailSent: true
-                    });
-                } else {
-                    this.setState({
-                        disabled: false,
-                        emailSent: false
-                    });
-                }
-            })
-            .catch(err => {
-                console.log(err);
-
-                this.setState({
-                    disabled: false,
-                    emailSent: false
-                });
-            });
-
-            this.clearForm();
-  }
-
-  clearForm =  () => {
-    this.setState(
-      {
-        name: '',
-        email: '',
-        subject: '',
-        message: '',
-      }
-    )
-  }
   
   
-    
-  
-render(){
-  
-  const { name, email, subject, message, errors} = this.state;
-  
-    return (
-        <>
-          <div className="contact-form">
-            <h1 className="contact-heading">Get in Touch</h1>
-          <Form onSubmit={this.handleSubmit}>
-            <FormGroup control_id="formBasicEmail">
-              <Label className="text-muted">Email address</Label>
-              <Input
-                type="email"
-                name="email"
-                value={email}
-                className="text-primary form_input"
-                onChange={this.change}
-                placeholder="Enter email"
-                errors={errors}
-              />
-            </FormGroup>
-            <FormGroup control_id="formBasicName">
-              <Label className="text-muted">Name</Label>
-              <Input
-                type="text"
-                name="name"
-                value={name}
-                className="text-primary form_input"
-                onChange={this.change}
-                placeholder="Name"
-              />
-            </FormGroup>      
-            <FormGroup control_id="formBasicSubject">
-              <Label className="text-muted">Subject</Label>
-              <Input
-                type="text"
-                name="subject"
-                className="text-primary form_input"
-                value={subject}
-                onChange={this.change}
-                placeholder="Subject"
-              />
-            </FormGroup>
-            <FormGroup control_id="formBasicMessage">
-              <Label className="text-muted">Message</Label>
-              <Input
-                type="textarea"
-                name="message"
-                className="text-primary form_input"
-                value={message}
-                onChange={this.change}
-                placeholder="Your special message to me"
-              />
-            </FormGroup>
-            <Button id="submit" variant="primary" type="submit" disabled={this.disabled}>
-              Submit
-            </Button>
-          </Form>
-          </div>
-        </>
-    )
-  }
+     return (
+       <div className="contact-form">
+       <h1 className="contact-heading">Get in Touch</h1>
+        <Form onSubmit={this.handleSubmit}>
+          <FormGroup control_id="formBasicEmail">
+            <Label className="text-muted">Email address</Label>
+            <Input
+              type="email"
+              name="email"
+              value=""
+              className="text-primary form_input"
+              onChange=""
+              placeholder="Enter email"
+              errors={error}
+            />
+          </FormGroup>
+        <FormGroup control_id="formBasicName">
+         <Label className="text-muted">Name</Label>
+         <Input
+           type="text"
+           name="name"
+           value={}
+           className="text-primary form_input"
+           onChange={}
+           placeholder="Name"
+         />
+       </FormGroup>      
+        <FormGroup control_id="formBasicSubject">
+         <Label className="text-muted">Subject</Label>
+         <Input
+           type="text"
+           name="subject"
+           className="text-primary form_input"
+           value={}
+           onChange={}
+           placeholder="Subject"
+         />
+       </FormGroup>
+        <FormGroup control_id="formBasicMessage">
+         <Label className="text-muted">Message</Label>
+         <Input
+           type="textarea"
+           name="message"
+           className="text-primary form_input"
+           value={}
+           onChange={}
+           placeholder="Your special message to me"
+         />
+       </FormGroup>
+       <Button id="submit" variant="primary" type="submit" disabled={}>
+         Submit
+       </Button>
+     </Form>
+     </div>
+     )
 }
+
+
 export default Contact

faeaadff11565ab1fbfd4dd36f1599efadc4ea2f
Kervintz Noel <kervintz84@hotmail.com>
2021-06-26 21:24:38 -0400

    fixing bugs in contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,146 +1,142 @@
-import React, { useState } from 'react';
+//esversion6 
+
+import React from 'react';
 import "./contact.styles.css";
 import axios from 'axios';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import useForm from './useForm';
-import validateForm from './validateForm'
 
-
-// function Contact() {
-
-//   const { handleChange, values, handleSubmit, errors } = useForm(validateForm)
-
-//   const [sent, setSent] = useState(false)
-//   const [text, setText] = useState("")
-//   const [error, setError] = useState("")
-  
-
-    
-//     try {
-//        axios.post("http://localhost:3003/send_mail", {
-//         text
-//       })
-//     } catch (error) {
-//       console.log(error)
-//     }
   
 class Contact extends React.Component {
 
-  constructor(props) {
-      super(props);
-      this.state = {
-        name: '',
-        email: '',
-        subject:'',
-        message: ''
-      }
+  state = {
+    name: '',
+    email: '',
+    subject:'',
+    message: ''
   }
+  
 
-  onNameChange(e) {
+  onNameChange = (e) => {
       this.setState({name: e.target.value})
   }
 
-  onEmailChange(e) {
+  onEmailChange= (e) => {
       this.setState({email: e.target.value})
   }
 
-  onSubjectChange(e) {
+  onSubjectChange= (e) => {
       this.setState({subject: e.target.value})
   }
 
-  onMsgChange(e) {
+  onMsgChange= (e) => {
       this.setState({message: e.target.value})
   }
 
-  submitEmail(e){
-    e.preventDefault();
-    axios({
-      method: "POST", 
-      url:"/send", 
-      data:  this.state
-    }).then((response)=>{
-      if (response.data.status === 'success'){
-          alert("Message Sent."); 
-          this.resetForm()
-      }else if(response.data.status === 'fail'){
-          alert("Message failed to send.")
-      }
-    })
-}
 
-resetForm(){
-    this.setState({name: '', email: '', subject:'', message: ''})
-}
+  resetForm = () => {
+    this.setState({
+      name: '',
+      email: '',
+      subject:'',
+      message: ''
+    })
+  }
+
+  submitEmail = (e) => {
+    e.preventDefault();
+
+   
+
+    const data = {
+      name: this.state.name,
+      email: this.state.email,
+      subject: this.state.subject,
+      message: this.state.message
+    };
+
+    console.log(data);
+
+    this.resetForm();
+
+    axios.post('http://localhost:5000/data', data)
+      .then(res => {
+        
+        console.log(res.data);
+      })
+      .catch(err => console.log(err))
+  }
 
   
-     render() {
+
+
+
+  
+  render() {
       return (
         <div className="contact-form">
-         <h1 className="contact-heading">Get in Touch</h1>
-           <Form onSubmit={this.submitEmail.bind(this)} method="POST">
+          <h1 className="contact-heading">Get in Touch</h1>
+            <Form onSubmit={this.submitEmail} method="POST">
               <FormGroup control_id="formBasicName">
               <div className="input-field">
                 <Label className="text-muted">Name</Label>
                 <Input
                   type="text"
                   name="name"
-                  value=""
                   id="name"
                   className="text-primary form_input"
                   placeholder="Name"
                   value={this.state.name}
-                  onChange={this.onNameChange.bind(this)}
+                  onChange={this.onNameChange}
                 />
               </div>
-       
-              {/*{errors.name && <p className="validation">{errors.name}</p>} */}
+        
             </FormGroup> 
-             <FormGroup control_id="formBasicEmail">
-               <div className="input-field">
-                 <Label className="text-muted">Email address</Label>
-                 <Input
-                   type="email"
-                   name="email"              
-                   id="email"
-                   className="text-primary form_input"
-                   placeholder="Enter email"
-                   value={this.state.email}
-                   onChange={this.onEmailChange.bind(this)}
-                   required
-                 />
-               </div>
-             {/*{errors.email && <p className="validation">{errors.email}</p>} */}
-           </FormGroup>
+              <FormGroup control_id="formBasicEmail">
+                <div className="input-field">
+                  <Label className="text-muted">Email address</Label>
+                  <Input
+                    type="email"
+                    name="email"              
+                    id="email"
+                    className="text-primary form_input"
+                    placeholder="Enter email"
+                    value={this.state.email}
+                    onChange={this.onEmailChange}
+                    required
+                  />
+                </div>
+              {/*{errors.email && <p className="validation">{errors.email}</p>} */}
+            </FormGroup>
             
-         <FormGroup control_id="formBasicSubject">
-             <div className="input-field">
-               <Label className="text-muted">Subject</Label>
-               <Input
-                 type="text"
-                 name="subject"
-                 id="subject"
-                 className="text-primary form_input"
-                 value={this.state.subject}
-                 onChange={this.onSubjectChange.bind(this)}
-                 placeholder="Subject"
-               />
-             </div>
+          <FormGroup control_id="formBasicSubject">
+              <div className="input-field">
+                <Label className="text-muted">Subject</Label>
+                <Input
+                  type="text"
+                  name="subject"
+                  id="subject"
+                  className="text-primary form_input"
+                  value={this.state.subject}
+                  onChange={this.onSubjectChange}
+                  placeholder="Subject"
+                />
+              </div>
           
           {/*{errors.subject && <p className="validation">{errors.subject}</p>} */}
         </FormGroup>
-         <FormGroup control_id="formBasicMessage">
-             <div className="input-field">
-               <Label className="text-muted">Message</Label>
-               <Input
-                 type="textarea"
-                 name="message"
-                 id="message"
-                 className="text-primary form_input"
-                 value={this.state.message}
-                 onChange={this.onMsgChange.bind(this)}
-                 placeholder="Your special message to me"
-               />
-             </div>
+          <FormGroup control_id="formBasicMessage">
+              <div className="input-field">
+                <Label className="text-muted">Message</Label>
+                <Input
+                  type="textarea"
+                  name="message"
+                  id="message"
+                  className="text-primary form_input"
+                  value={this.state.message}
+                  onChange={this.onMsgChange}
+                  placeholder="Your special message to me"
+                />
+              </div>
           
           {/*{errors.message && <p className="validation">{errors.message}</p>} */}
         </FormGroup>
@@ -150,8 +146,7 @@ resetForm(){
       </Form>
       </div>
       )
-
-     }
+  }
 }
 
 

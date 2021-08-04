c18c48031fa06ffbed0bf998335ca19df78c0468
Kervintz Noel <kervintz84@hotmail.com>
2021-08-03 23:51:17 -0400

    rewrite contact page

---
+++ src/components/Contact2.js
@@ -0,0 +1,32 @@
+
+import React, { Component } from 'react';
+import Pdf from '../Resume - KervintzNoel.pdf';
+import "./contact.styles.css";
+
+class Contact2 extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Contact info</h1>
+                <div className="contact-form">
+                    <div className="contact-item tel">
+                        <a href="tel:786-923-6596" alt="">786-923-6596</a>
+                    </div>
+
+                    <div className="contact-item email">
+                        <a href=" mailto: kervcodes@gmail.com" alt="">kervcodes@gmail.com</a>
+                    </div>  
+                    <div className="contact-item">
+                        <a href="https://www.linkedin.com/in/kervintznoel/" target="_blank" rel="noopener noreferrer" alt="">LinkedIn</a>
+                    </div>
+
+                    <div className="contact-item">
+                        <a href = {Pdf} target = "_blank" rel="noopener noreferrer">Resume</a>                  
+                    </div>                  
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Contact2
